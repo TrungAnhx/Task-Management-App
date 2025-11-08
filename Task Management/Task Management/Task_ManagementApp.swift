3c49f668e89500ee15b9179d349c555136e9c5d6
Trungkun103 <trungkun103@gmail.com>
2025-11-08 21:48:16 +0700

    checked

---
+++ Task Management/Task Management/Task_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementApp.swift
+//  Task Management
+//
+//  Created by TrungAnhx on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Task Management/Task ManagementTests/Task_ManagementTests.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagementTests.swift
+//  Task ManagementTests
+//
+//  Created by TrungAnhx on 8/11/25.
+//
+
+import Testing
+@testable import Task_Management
+
+struct Task_ManagementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
